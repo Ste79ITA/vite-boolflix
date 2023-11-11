44d08411ab7bf7736d9c8c70865fe78bb97dad72
Ste79ITA <nastri.s@gmail.com>
2023-11-11 20:44:49 +0100

    basic layout

---
+++ src/components/AppCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div>
+    <section>
+      <main class="main-section">
+        <h1>RESULTS</h1>
+      </main>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.main-section {
+  max-width: 700px;
+  background-color: grey;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  border-radius: 15px;
+}
+</style>
--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,21 +1,58 @@
 <script>
 import { store } from '../store.js';
-import axios from 'axios';
+import AppCard from './AppCard.vue';
+
+export default {
+  data() {
+    return { store };
+  },
+  components: { AppCard },
+  methods: {
+    search() {
+      console.log(store.searchText);
+    },
+  },
+  updated() {},
+};
 </script>
 
 <template>
-  <div>
-    <section>
-      <main>
-        <input type="text" placeholder="Cerca un film o una serie Tv" />
-      </main>
-      <button>Search</button>
-    </section>
+  <div class="section">
+    <main class="main-section">
+      <input
+        v-model="this.store.searchText"
+        type="text"
+        placeholder="Cerca un film o una serie Tv"
+      />
+      <button @click="search()">Search</button>
+    </main>
+    <div class="card-container">
+      <AppCard />
+    </div>
   </div>
 </template>
 
 <style scoped>
-.main {
+.section {
+  padding-top: 50px;
+}
+.main-section {
+  max-width: 700px;
+  background-color: grey;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 0;
+  border-radius: 15px;
+}
+
+.card-container {
   border: 1px solid greenyellow;
+  max-width: 750px;
+  margin: 0 auto;
+  margin-top: 50px;
+  padding-bottom: 50px;
+  border-radius: 20px;
 }
 </style>
