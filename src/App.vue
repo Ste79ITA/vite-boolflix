f6b2ec4f8b4f0d913fdf0bf356199814af27258e
Ste79ITA <nastri.s@gmail.com>
2023-11-11 19:20:30 +0100

    Folder Setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from './store.js';
+import PageMain from './components/PageMain.vue';
+
+export default {
+  components: {
+    PageMain,
+  },
+  data() {
+    return { store };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <PageMain />
+  </div>
+</template>
+
+<style lang="scss">
+.main {
+  border: 1px solid greenyellow;
+}
+</style>
---
+++ src/components/PageMain.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from '../store.js';
+import axios from 'axios';
+</script>
+
+<template>
+  <div>
+    <section>
+      <main>
+        <input type="text" placeholder="Cerca un film o una serie Tv" />
+      </main>
+      <button>Search</button>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  border: 1px solid greenyellow;
+}
+</style>
